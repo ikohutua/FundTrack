f8b9b1ba79b2b3a0c75bd0c06cf5ce58e1ce8e6b
Viktoriia Zheliezniak <TRAINING\vzhelitc>
2017-07-02 23:03:40 +0300

    Add missing file.

---
+++ FundTrack.WebUI/ClientApp/app/view-models/abstract/organization-registration-view-model.interface.ts
@@ -0,0 +1,12 @@
+﻿export interface IOrganizationRegistrationViewModel {
+    id: number; 
+    name: string;
+    description: string; 
+    city: string; 
+    country: string; 
+    street: string; 
+    house: string; 
+    administratorLogin: string;
+    userError: string;
+    nameError: string;
+    }
