3b0661756fdc62e0d2e9a0edc0600677b4a68f01
Oksana Medvid' <TRAINING\omedvitc>
2017-07-17 14:26:15 +0300

    status

--- FundTrack.Infrastructure/Status.cs
+++ FundTrack.Infrastructure/Status.cs
@@ -12,26 +12,26 @@ namespace FundTrack.Infrastructure
         /// <summary>
         /// The new
         /// </summary>
-        public const string New = "new";
+        public const string New = "Новий";
 
         /// <summary>
         /// The active
         /// </summary>
-        public const string Active = "active";
+        public const string Active = "Активний";
 
         /// <summary>
         /// The in active
         /// </summary>
-        public const string InActive = "inactive";
+        public const string InActive = "Неактивний";
 
         /// <summary>
         /// The close
         /// </summary>
-        public const string Close = "close";
+        public const string Close = "Виконаний";
 
         /// <summary>
         /// The in progress
         /// </summary>
-        public const string InProgress = "inProgress";
+        public const string InProgress = "В процесі";
     }
 }
