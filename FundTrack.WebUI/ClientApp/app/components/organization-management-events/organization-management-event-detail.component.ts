4a79d487caf4da21afda00cbb12372eb3140b64d
Borys Malynovs'kyi <TRAINING\bmalytc>
2017-07-23 00:02:42 +0300

    Add event detail

---
+++ FundTrack.WebUI/ClientApp/app/components/organization-management-events/organization-management-event-detail.component.ts
@@ -0,0 +1,56 @@
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { OrganizationManagementEventsService } from "../../services/concrete/organization-management/organization-management-events.service";
+import { IEventManagementViewModel } from "../../view-models/abstract/organization-management-view-models/event-management-view-model.interface";
+import { Subscription } from "rxjs/Subscription";
+
+@Component({
+    selector: 'org-management-event-detail',
+    templateUrl: './organization-management-event-detail.component.html',
+    styles: ['./organization-management-event-detail.component.css']
+})
+export class OrganizationManagementEventDetailComponent {
+    private _idForCurrentEvent: number;
+    private _event: IEventManagementViewModel;
+    private _subscription: Subscription;
+    private _images: string[];
+
+    /**
+     * @constructor
+     * @param _route: ActivatedRoute
+     * @param _router: Router
+     * @param _service: OrganizationManagementEventsService
+     */
+    public constructor(private _route: ActivatedRoute, private _router: Router, private _service: OrganizationManagementEventsService) { }
+
+    ngOnInit(): void {
+        this._subscription = this._route.params.subscribe(params => {
+            this._idForCurrentEvent = +params['id'];
+            this.getInformationOfEvent(this._idForCurrentEvent);
+        });
+    }
+
+    /**
+    * Gets one event by identifier
+    * @param id
+    */
+    private getInformationOfEvent(id: number): void {
+        debugger;
+        this._service.getOneEventById(id).subscribe(
+            event => {
+                debugger;
+                this._event = event;
+                this._images = [];
+                this._event.images.forEach((image) => {
+                    this._images.push(image.imageUrl);
+                });
+            });
+    }
+
+    /**
+     * Redirect to all events list
+     */
+    private redirectToAllEvents(): void {
+        this._router.navigate(['organization/events/' + this._event.organizationId]);
+    }
+}
