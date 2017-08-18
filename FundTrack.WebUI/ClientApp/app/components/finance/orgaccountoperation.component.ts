f433004cbf179643db6048cc66cb191893de9123
Taras Vytrykush <TRAINING\tvytryktc>
2017-08-18 11:48:30 +0300

    AddedImputProp

--- FundTrack.WebUI/ClientApp/app/components/finance/orgaccountoperation.component.ts
+++ FundTrack.WebUI/ClientApp/app/components/finance/orgaccountoperation.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit, Input } from "@angular/core";
+﻿import { Component, OnInit, Input, SimpleChange, OnChanges } from "@angular/core";
 import { Router } from "@angular/router";
 import { OrgAccountService } from "../../services/concrete/finance/orgaccount.service";
 import { OrgAccountViewModel } from "../../view-models/concrete/finance/orgaccount-viewmodel";
@@ -11,14 +11,24 @@ import { CurrencyPipe } from '@angular/common';
     templateUrl: './orgaccountoperation.component.html',
     styleUrls: ['./orgaccountoperation.component.css']
 })
-export class OrgAccountOperationComponent {
+export class OrgAccountOperationComponent implements OnChanges {
     @Input('orgId') orgId: number;
+    @Input() accountId: number;
 
     constructor(private _router: Router) {
 
     }
     private navigateToImportsPage(): void {
         this._router.navigate(['/finance/bank-import']);
+    }
+
+    /*
+    Checks for value changes and assignes new account in the component
+    */
+    ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+        if (changes['accountId'] && changes['accountId'] != changes['accountId'].currentValue) {
+            //code to execute when property changes
+        }
     }
 
 }
