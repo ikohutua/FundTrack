40a081baec43e1ba15e7d3e76d8fe5ca6ee9ed73
Taras <tshuber96@gmail.com>
2017-09-13 12:52:49 +0300

    Add files via upload

---
+++ TestEneida/TestTfsConsoleApplication/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void testMethod()
+        {
+            Console.WriteLine("Hello from test method from Bohdan Lyesiko");
+        }
+
+        static void Main(string[] args)
+        {
+            testMethod();
+            Console.WriteLine("Who am i? I`m a champion!!");//Dima
+            Console.WriteLine("Hello from Bohdan Lyseiko and " + DimaSaySomething());
+            Console.WriteLine("Hello from Bohdan Lyseiko. asdflsfnckl");
+            Console.WriteLine();
+            TarasSaysSomething();
+            Console.WriteLine("Input name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("First Comment");
+            Console.WriteLine("Second Comment");
+            Console.WriteLine("Thirth Name");
+            Console.WriteLine("Fourth Comment");
+            Console.WriteLine("Fifth Comment");
+            Console.Read();
+        }
+
+        static void testMethod2()
+        {
+            Console.WriteLine("Hello from test method from Bohdan Lyesiko 2");
+        }
+        private static string DimaSaySomething()
+        {
+            return "Keep calm and write code like a hero))";
+        }
+
+        private static void TarasSaysSomething()
+        {
+            Console.WriteLine("Test hello from Taras");
+        }
+
+    }
+}
