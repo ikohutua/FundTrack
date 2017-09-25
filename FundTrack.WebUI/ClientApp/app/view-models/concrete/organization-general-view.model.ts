fa3452ca105ecfcade7f663da3a48f765cb81f5e
Bogdan <lyseiko.bogdan@gmail.com>
2017-09-25 19:05:34 +0300

    added filtering for organization list

--- FundTrack.WebUI/ClientApp/app/view-models/concrete/organization-general-view.model.ts
+++ FundTrack.WebUI/ClientApp/app/view-models/concrete/organization-general-view.model.ts
@@ -1,4 +1,6 @@
-﻿export class OrganizationGeneralViewModel {
+﻿import { IOrganizationForFiltering } from "../abstract/organization-for-filtering.interface";
+
+export class OrganizationGeneralViewModel implements IOrganizationForFiltering {
     id: number;
     name: string; 
     description: string;
