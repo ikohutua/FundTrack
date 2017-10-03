b1156243f8713533fd94672718640593ce365221
Bogdan <lyseiko.bogdan@gmail.com>
2017-10-03 13:04:42 +0300

    small changes

--- FundTrack.WebUI/ClientApp/app/services/concrete/global-url.service.ts
+++ FundTrack.WebUI/ClientApp/app/services/concrete/global-url.service.ts
@@ -1,6 +1,7 @@
 ﻿import { Injectable, Inject, NgZone } from '@angular/core';
 
 @Injectable()
+export class GlobalUrlService {
     // urls to server for all components
     public static getAllOrganizationsUrl: string = "api/OrganizationDetail";
     public static getOrganizationDetailUrl: string = "api/OrganizationDetail/OrganinzationDetailByOrgId/";
