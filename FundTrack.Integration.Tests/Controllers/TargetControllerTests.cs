c237c18aa37918c072f744f1160d580ebea7d59c
Andriy Yavorskiy <andriy.yavorskiy21@gmail.com>
2017-11-11 22:03:36 +0200

    add testContext, TargetContollerTests

---
+++ FundTrack.Integration.Tests/Controllers/TargetControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using FundTrack.DAL.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Target = FundTrack.DAL.Entities.Target;
+
+namespace FundTrack.Integration.Tests.Controllers
+{
+    public class TargetControllerTests
+    {
+        private readonly TestContext _testContext;
+
+        public TargetControllerTests()
+        {
+            _testContext = new TestContext();
+        }
+
+        private IQueryable<Target> GetTestTargets()
+        {
+            return new List<Target>()
+            {
+                new Target(){ Id = 1, OrganizationId = 1, TargetName = "Продукти" },
+                new Target(){ Id = 2, OrganizationId = 2, TargetName = "Ліки" },
+                new Target(){ Id = 3, OrganizationId = 3, TargetName = "Одяг" },
+                new Target(){ Id = 4, OrganizationId = 4, TargetName = "Електроніка" },
+                new Target(){ Id = 5, OrganizationId = 5, TargetName = "Боєприпаси" }
+            }.AsQueryable();
+        }
+
+        private Target GetTestTargetById(int id)
+        {
+            return GetTestTargets().Where(t => t.Id == id).Single();
+        }
+
+        [Fact]
+        public async Task Get_Target_Bu_Id_Ok_Response()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<FundTrackContext>()
+            .UseInMemoryDatabase(databaseName: "Get_Target_Bu_Id_Ok_Response")
+            .Options;
+
+            var testTarget = GetTestTargetById(1);
+            using (var context = new FundTrackContext(options))
+            {
+                context.Targets.Add(testTarget);
+                context.SaveChanges();
+            }
+
+            //Act
+            var response = await _testContext.Client.GetAsync($"/api/Target/GetTarget/{testTarget.Id}");
+            var stream = await response.Content.ReadAsStreamAsync();
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            var resultTarget = new JsonSerializer().Deserialize<Target>(new JsonTextReader(reader));
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(testTarget.Id, resultTarget.Id);
+            Assert.Equal(testTarget.TargetName, resultTarget.TargetName);
+            Assert.Equal(testTarget.OrganizationId, resultTarget.OrganizationId);
+            Assert.True(HttpStatusCode.OK == response.StatusCode);
+        }
+    }
+}
---
+++ FundTrack.Integration.Tests/TestContext.cs
@@ -0,0 +1,35 @@
+﻿using FundTrack.WebUI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace FundTrack.Integration.Tests
+{
+    public class TestContext : IDisposable
+    {
+        private TestServer _server;
+        public HttpClient Client { get; private set; }
+
+        public TestContext()
+        {
+            SetUpClient();
+        }
+
+        private void SetUpClient()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
